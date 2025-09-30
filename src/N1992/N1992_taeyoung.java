a43ef0f0fdd98d596d9b4eef8c39761e6cd632dd
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-09-30 17:30:34 +0900

    쿼드트리 / 실버 1 / 112ms

---
+++ src/N1992/N1992_taeyoung.java
@@ -0,0 +1,78 @@
+package N1992;
+
+import java.io.*;
+import java.util.*;
+
+public class N1992_taeyoung {
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] A = new int[N][N]; // 초기 배열
+
+		for (int i = 0; i < N; i++) {
+			String S = br.readLine();
+			for (int j = 0; j < N; j++) {
+				A[i][j] = S.charAt(j) - '0';
+			}
+		}
+
+		sol(A);
+
+		System.out.println(sb); // 정답 출력
+
+	}
+
+	// 정답을 구하기 위한 재귀 함수
+	static void sol(int[][] arr) {
+		if (check(arr)) { // 재귀 종료 조건 -> 하나의 수로 이루어졌다면
+			return;
+		}
+		int l = arr.length; // 배열 크기
+		
+		sb.append('('); // 재귀 호출 시 ( 추가
+		
+		// 4등분
+		for (int i = 0; i < l; i += l/2) {
+			for (int j = 0; j < l; j += l/2) {
+				// 4등분 위한 임시 배열
+				int[][] tmp = new int[l/2][l/2]; // 행, 열 크기 1/2
+				for (int r = 0; r < l / 2; r++) {
+					for (int c = 0; c < l / 2; c++) {
+						tmp[r][c] = arr[i + r][j + c]; // i,j -> 시작 위치(좌상단) / r,c -> 시작점에서 상대적인 위치
+					}
+				}
+				sol(tmp); // 분할한 배열로 재귀 호출
+			}
+		}
+		
+		// 재귀 호출 종료 시 ) 추가
+		sb.append(')');
+	}
+
+	// 주어진 배열이 하나의 수로 이루어졌는지 확인하는 함수
+	static boolean check(int[][] arr) {
+		int l = arr.length; // 배열의 크기
+		int n = arr[0][0]; // 비교를 위한 배열 첫 원소
+
+		for (int i = 0; i < l; i++) {
+			for (int j = 0; j < l; j++) {
+				if (arr[i][j] != n) // 배열을 탐색하며 다른 수가 나오면 false
+					return false;
+			}
+		}
+		// 2중 for 문 통과 -> 하나의 수로 이루어짐
+		switch (n) { // 어떤 수인지에 따라 sb에 추가한다
+		case 1:
+			sb.append(1);
+			break;
+		case 0:
+			sb.append(0);
+			break;
+		}
+		return true; // true 반환
+	}
+}
