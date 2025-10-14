3a71565a0d8473224fbf4bd092f874375481c081
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-10-14 15:15:19 +0900

    N과 M (9) / 실버 2 / 176ms

---
+++ src/N15663/N15663_taeyoung.java
@@ -0,0 +1,61 @@
+package N15663;
+
+import java.io.*;
+import java.util.*;
+
+public class N15663_taeyoung {
+	static int[] A, nums; // nums: 주어진 자연수 배열
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+	static boolean[] checked;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 사전 순 -> 정렬
+		Arrays.sort(nums);
+
+		A = new int[M];
+		checked = new boolean[N];
+
+		backtracking(0);
+
+		System.out.println(sb);
+	}
+
+	// 백트래킹 함수
+	static void backtracking(int x) {
+		if (x == M) {
+			for (int a : A) {
+				sb.append(a).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		// 주어진 수에 중복되는 수 존재
+		// 현재 깊이에서 같은 수를 고르면 안된다
+		int prev = -1;
+		for (int i = 0; i < N; i++) {
+			// N개의 수 중에서 M개 고름 -> 사용 체크 필요
+			// 중복 수열 출력 X -> prev와 같은 값은 넘어간다
+			if (!checked[i] && nums[i] != prev) {
+				A[x] = nums[i];
+				checked[i] = true;
+				backtracking(x + 1);
+				checked[i] = false;
+				prev = nums[i]; // prev 갱신
+			}
+		}
+	}
+}
