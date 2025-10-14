6bbbe06381f209f58d45b6728f85dd6e3b244078
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-10-14 15:32:28 +0900

    N과 M (11) / 실버 2 / 312ms

---
+++ src/N15665/N15665_taeyoung.java
@@ -0,0 +1,57 @@
+package N15665;
+
+import java.io.*;
+import java.util.*;
+
+public class N15665_taeyoung {
+	static int[] A, nums; // nums: 주어진 자연수 배열
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 사전 순 -> 정렬
+		Arrays.sort(nums);
+
+		A = new int[M];
+
+		backtracking(0);
+
+		System.out.println(sb);
+	}
+
+	// 백트래킹 함수
+	static void backtracking(int x) {
+		if (x == M) {
+			for (int a : A) {
+				sb.append(a).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		// 주어진 수에 중복되는 수 존재
+		// 현재 깊이에서 같은 수를 고르면 안된다
+		int prev = -1;
+		for (int i = 0; i < N; i++) {
+			// 중복 수열 출력 X -> prev와 같은 값은 넘어간다
+			// 같은 수 여러 번 골라도 됨 -> 다른 조건문 필요 없다
+			if (nums[i] != prev) {
+				A[x] = nums[i];
+				backtracking(x + 1);
+				prev = nums[i];
+			}
+		}
+	}
+}
