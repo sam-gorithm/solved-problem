920a7d6349735feb44ce225fab1c2900109f3315
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-10-15 03:53:57 +0900

    N과 M(8) / 실버 3 / 188ms

---
+++ src/N15657/N15657_seoyeon.java
@@ -0,0 +1,55 @@
+package N15657;
+
+import java.io.*;
+import java.util.*;
+
+public class N15657_seoyeon {
+
+    static int N, M;
+    static int arr[];
+    static int input[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        input = new int[N];
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+
+        backTrack(0, 0);
+        System.out.println(sb.toString());
+
+    }
+
+    private static void backTrack(int depth, int next) {
+        // base condition
+        if (depth == M) {
+            for (int a : arr) {
+                sb.append(a + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        // 재귀부
+        for (int i = next; i < N; i++) {
+
+            arr[depth] = input[i];
+            backTrack(depth + 1, i);
+
+        }
+
+    }
+
+}
+
