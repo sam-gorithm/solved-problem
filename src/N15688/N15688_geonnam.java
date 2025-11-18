dbbbb11a06e096a6bc9e95fa5640963669d6254c
KGN0826 <rjsska8026@gmail.com>
2025-11-19 02:29:39 +0900

    수 정렬하기 5 / 실버 5 / 19436ms

---
+++ src/N15688/N15688_geonnam.java
@@ -0,0 +1,31 @@
+package N15688;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class N15688_geonnam {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i=0;i<N;i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list); // 오름차순 정렬
+		
+		for (int a : list) {
+			sb.append(a).append("\n"); //출력 형식 맞추기
+		}
+		
+		System.out.println(sb);
+	}
+}
