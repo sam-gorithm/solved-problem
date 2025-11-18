434c932ac5bec6658b5f6be21deb801ab5398d35
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-11-18 22:56:59 +0900

    수 정렬하기 5 / 실버 5 / 18288ms

---
+++ src/N15688/N15688_taeyoung.java
@@ -0,0 +1,31 @@
+package N15688;
+
+import java.util.*;
+import java.io.*;
+
+public class N15688_taeyoung {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] A = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(A); // 정렬
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < N; i++) {
+			sb.append(A[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
+
