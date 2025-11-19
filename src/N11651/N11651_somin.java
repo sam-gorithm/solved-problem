dc1dc3800615a24259e4092f580c63ac3ed612a2
YooSoMin <somin3994@naver.com>
2025-11-19 09:23:28 +0900

    좌표 정렬하기 2 / 실버 5 / 584ms

---
+++ src/N11651/N11651_somin.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N11651_somin {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int [N][2];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken()); // X좌표
+            arr[i][1] = Integer.parseInt(st.nextToken()); // Y좌표
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            } else {
+                return e1[1] - e2[1];
+            }
+        });
+
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
